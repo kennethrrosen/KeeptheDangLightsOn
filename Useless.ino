e792edeb650f53932d6cc742ecf9b3f4f68e1e1e
Oops <questionable.mechanics@gmail.com>
2021-03-02 11:55:48 +0100

    Update Useless.ino

--- Useless.ino
+++ Useless.ino
@@ -27,57 +27,7 @@ const int switchPin = 2;
 const int DS1307 = 0x68;
 
 int state = 0;
-/*
-byte verticalLine[8] = {
-  B00100,
-  B00100,
-  B00100,
-  B00100,
-  B00100,
-  B00100,
-  B00100,
-  B00100
-};
-byte char2[8] = {
-  B00000,
-  B00000,
-  B00000,
-  B11100,
-  B00100,
-  B00100,
-  B00100,
-  B00100
-};
-byte char1[8] = {
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00111,
-  0b00100,
-  0b00100,
-  0b00100,
-  0b00100
-};
-byte char3[8] = {
-  0b00100,
-  0b00100,
-  0b00100,
-  0b00111,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000
-};
-byte char4[8] = {
-  0b00100,
-  0b00100,
-  0b00100,
-  0b11100,
-  0b00000,
-  0b00000,
-  0b00000,
-  0b00000
-};
+
 */
 byte DownArrow[8] = {
   B00100,
@@ -122,31 +72,7 @@ byte Skull[8] = {
   B00000,
   B00000
 };
-/*
-void printFrame()
-{
-  lcd.setCursor(1, 0);
-  lcd.print("------------------");
-  lcd.setCursor(1, 3);
-  lcd.print("------------------");
-  lcd.setCursor(0, 1);
-  lcd.write(byte(0));
-  lcd.setCursor(0, 2);
-  lcd.write(byte(0));
-  lcd.setCursor(19, 1);
-  lcd.write(byte(0));
-  lcd.setCursor(19, 2);
-  lcd.write(byte(0));
-  lcd.setCursor(0, 0);
-  lcd.write(byte(1));
-  lcd.setCursor(19, 0);
-  lcd.write(byte(2));
-  lcd.setCursor(0, 3);
-  lcd.write(byte(3));
-  lcd.setCursor(19, 3);
-  lcd.write(byte(4));
-}
-*/
+
 void createCustomCharacters()
 {
   //lcd.createChar(0, verticalLine);
